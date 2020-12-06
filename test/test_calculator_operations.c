ff7f32dd75c17a53d40045e75b6caeb1286a3696
99003164 <75420607+99003164@users.noreply.github.com>
2020-12-06 21:17:17 +0530

    Update test_calculator_operations.c

--- test/test_calculator_operations.c
+++ test/test_calculator_operations.c
@@ -244,50 +244,50 @@ void test_inchtocenti(void) {
 }
 
 void test_and(void) {
- CU_ASSERT(10 ==  and (10, 15));
+ CU_ASSERT(10 ==  and(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == and (8, 15));
+ CU_ASSERT(9 == and(8, 15));
 }
 
 void test_or(void) {
- CU_ASSERT(15 ==  or (10, 15));
+ CU_ASSERT(15 ==  or(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == or (10, 10));
+ CU_ASSERT(9 == or(10, 10));
 }
 
 void test_not(void) {
- CU_ASSERT(-36 ==  not (35));
+ CU_ASSERT(-36 ==  not(35));
  
  /* Dummy fail*/
- CU_ASSERT(9 == not (-12));
+ CU_ASSERT(9 == not(-12));
 }
 
 void test_nand(void) {
- CU_ASSERT(-11 ==  nand (10, 15));
+ CU_ASSERT(-11 ==  nand(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == nand (8, 15));
+ CU_ASSERT(9 == nand(8, 15));
 }
 
 void test_nor(void) {
- CU_ASSERT(-16 ==  nor (10, 15));
+ CU_ASSERT(-16 ==  nor(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == nor (8, 15));
+ CU_ASSERT(9 == nor(8, 15));
 }
 
 void test_xor(void) {
- CU_ASSERT(5 ==  xor (10, 15));
+ CU_ASSERT(5 ==  xor(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == xor (8, 15));
+ CU_ASSERT(9 == xor(8, 15));
 }
 
 void test_xnor(void) {
- CU_ASSERT(-6 ==  xnor (10, 15));
+ CU_ASSERT(-6 ==  xnor(10, 15));
  
  /* Dummy fail*/
- CU_ASSERT(9 == xnor (8, 15));
+ CU_ASSERT(9 == xnor(8, 15));
 }
